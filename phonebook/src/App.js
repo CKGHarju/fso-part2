818e10537d0b0c2107e179a85f25deb592c4669a
CKGHarju <christopher.harju@devex.com>
2020-05-26 10:37:56 +0300

    2.9 phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ])
+  const [ filter, setFilter ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
 
   const styles = {
     label: { display: 'block' }
@@ -28,10 +32,16 @@ const App = () => {
     <p>{person.name} {person.number}</p>
   )
 
+  const personsToShow = filter ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase(), 0)): persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <label htmlFor="filter" style={styles.label}>
+        filter shown with: <input type='text' id='filter' value={filter} onChange={e => setFilter(e.target.value)}/>
+      </label>
       <form onSubmit={handleSubmit}>
+        <h2>Add a new</h2>
         <label htmlFor="name" style={styles.label}>
           Name: <input type='text' id='name' value={newName} onChange={e => setNewName(e.target.value)}/>
         </label>
@@ -47,7 +57,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <Person key={person.name} person={person}/>)}
+        {personsToShow.map(person => <Person key={person.name} person={person}/>)}
       </div>
     </div>
   )
